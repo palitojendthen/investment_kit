9cea83313aa680d4f4f91c66f98a43eb807f882f
Palito J. Endthen <94058918+palitojendthen@users.noreply.github.com>
2022-12-28 20:05:33 +0700

    Create portfolio_management.py

---
+++ python/portfolio_management.py
@@ -0,0 +1,38 @@
+
+"""
+Library
+"""
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import math
+import scipy.stats
+import statsmodels.stats.moment_helpers as mh
+from scipy.stats import norm
+from scipy.optimize import minimize
+from numpy.linalg import inv
+import plotly.express as px
+import yfinance as yf
+
+
+
+"""
+Portfolio Management/Optimization
+"""
+# Rate of Return
+def rate_of_return(x):
+    """
+    compute rate of returns of a time series data
+    """
+    ret = ((x[:-1].values / x[1:]).values - 1).round(2)
+    return np.append(np.nan, ret)
+
+
+# Compounding Returns
+def compound(r):
+    """
+    compute compounding or geometric mean returns 
+    for returns series
+    """
+    return np.expm1(np.log1p(r).sum())
+ 
