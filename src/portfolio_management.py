1dcd1fb6ebbda4d9bb58cb1adf12be2d9e6d1f33
Palito J. Endthen <94058918+palitojendthen@users.noreply.github.com>
2024-11-22 18:31:38 +0700

    Update portfolio_management.py

--- src/portfolio_management.py
+++ src/portfolio_management.py
@@ -58,45 +58,59 @@ def semi_deviation(r):
     return semi deviation,
     i.e. deviation from returns that below average or below zero
     params:
-    @r: returns time-series data
+    @r: series, returns over time, of a time-series input data
+    example:
+    >>> arr = np.random.randint(10, 30, 20)
+    >>> df = pd.DataFrame(arr, columns = ['close'])
+    >>> portfolio_management.semi_deviation(df.pct_change()) 
     """
     is_negative = r < 0
     return r[is_negative].std(ddof = 0)
 
 def skewness(r):
     """
-    return skewness of the supplied series data,
-    i.e. alternative to scipy.stats.skew
-    returns a float or a series
+    return skewness,
+    alternative to scipy.stats.skew
     params:
-    @r: returns time-series data
+    @r: series, returns over time, of a time-series input data
+    example:
+    >>> arr = np.random.randint(10, 30, 20)
+    >>> df = pd.DataFrame(arr, columns = ['close'])
+    >>> portfolio_management.skewness(df.pct_change())
     """
-    demeaned_r = r - r.mean()
+    demeaned_r = r-r.mean()
     sigma_r = r.std(ddof = 0)
     exp = (demeaned_r**3).mean()
     return exp/sigma_r**3
 
 def kurtosis(r):
     """
-    return kurtosis value of the supplied series data,
-    i.e. alternative to scipy.stats.kurtosis
-    returns a float or a series
+    return kurtosis,
+    alternative to scipy.stats.kurtosis
     params:
-    @r: returns time-series data
+    @r: series, returns over time, of a time-seires input data
+    example:
+    >>> arr = np.random.randint(10, 30, 20)
+    >>> df = pd.DataFrame(arr, columns = ['close'])
+    >>> portfolio_management.kurtosis(df.pct_change())
     """
-    demeaned_r = r - r.mean()
+    demeaned_r = r-r.mean()
     sigma_r = r.std(ddof = 0)
     exp = (demeaned_r**4).mean()
     return exp/sigma_r**4
 
 def is_normal(r, level = .01):
     """
-    return applies jarque-bera test to determine if a series is normaly distributed or not
-    test is applied at the 1% level by default
-    true if the hypothesis of normality is accepted, false otherwise
+    return applies jarque-bera test to determine if a series is normaly distributed or not,
+    test is applied at the 1% level by default,
+    true if the hypothesis of normality is accepted, false otherwise,
     params:
-    @r: returns time-series data
-    @level: confidence levels
+    @r: series, returns over time, of a time-series input data
+    @level: float, levels of confidence
+    example:
+    >>> arr = np.random.randint(10, 30, 20)
+    >>> df = pd.DataFrame(arr, columns = ['close'])
+    >>> portfolio_management.is_normal(df.pct_change())
     """
     statistic, p_value = scipy.stats.jarque_bera(r)
     return p_value > level
@@ -104,10 +118,15 @@ def is_normal(r, level = .01):
 def var_historic(r, level = 5):
     """
     return historic value-at-risk,
-    which means 95% i.e. level of confidence, that at worst
-    the estimated price performance would drop at specified level
-    @r: returns time-series data
-    @level: confidence levels
+    e.g. means at 95% level of confidence the risk associated,
+    estimated price performance would drop at specified level
+    params:
+    @r: series, returns over time, of a time-series input data
+    @level: integer, confidence level
+    example:
+    >>> arr = np.random.randint(10, 30, 20)
+    >>> df = pd.DataFrame(arr, columns = ['close'])
+    >>> portfolio_management.var_historic(df.pct_change().dropna())
     """
     if isinstance(r, pd.DataFrame):
         return r.aggregate(var_historic, level = level)
@@ -120,10 +139,22 @@ def annualize_rets(r, periods_per_year=12):
     """
     return annualized set of returns,
     params:
-    @r: returns time-series data
-    @periods per year: default to 12 months
-    
+    @r: series, returns over time, of a time-series input data
+    @periods_per_year: integer, number of n per periods
+    example:
+    >>> portfolio_management.annualize_rets(df['close'].pct_change().dropna())
     """
     compounded_growth = (1+r).prod()
     n_periods = r.shape[0]
     return compounded_growth**(periods_per_year/n_periods) - 1
+
+def annualize_vol(r, periods_per_year = 12):
+    """
+    return annualized volatility set of returns,
+    params:
+    @r: series, returns over time, of a time-series input data
+    @periods_per_year: integer, number of n per periods
+    example:
+    >>> portfolio_management.annualize_vol(df['close'].pct_change().dropna())
+    """
+    return r.std()*(periods_per_year**.5)
