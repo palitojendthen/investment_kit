6e28c4097c7e11227760e8157186d5c72721663b
Palito J. Endthen <94058918+palitojendthen@users.noreply.github.com>
2024-11-23 15:44:15 +0700

    create example applied Kaufman Adaptive Moving Average indicator

---
+++ examples/kama.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#load library
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+import sys
+path = r'path_to_cloned_repository'
+sys.path.insert(0, path)
+import technical_indicator
+
+# retrieve data
+start_date = '2022-01-01'; end_date = '2024-11-01'
+df = yf.download(tickers='BTC-USD', threads=True, start=start_date, end=end_date)
+df.columns = df.columns.str.lower().str.replace(' ','_')
+
+# apply kaufman adaptive moving average indicator
+kama = technical_indicator.kama(df['close'], return_df=True)
+kama['trigger'] = np.where((kama['kama'] > kama['kama'].shift(1)), 1, 0)
+kama['buy'] = np.where(kama['trigger'] == 1, kama['kama'], np.nan)
+kama['sell'] = np.where(kama['trigger'] == 0, kama['kama'], np.nan)
